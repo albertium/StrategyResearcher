0aea23ba362a1bc98c3c46dfd0600961ad76b8ed
wonalbe <albertium.land@gmail.com>
2018-10-27 00:41:23 -0400

    backup before revamp

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from strategyrunner.execution import SimulatedExecutionHandler
 from strategyrunner import Trader, Portfolio
 import time
 
-# bt = Trader(MomentumStrategy, Portfolio, HistoricalDataHandler, SimulatedExecutionHandler)
-bt = Trader(BuyAndHold, Portfolio, HistoricalDataHandler, SimulatedExecutionHandler)
-# bt.set_tickers(["CWI", "HYG", "IAU", "ITB", "SMH", "TLT", "VGK", "VOO", "XBI", "XLI", "XLV"])
-bt.set_tickers(['VFINX'])
-bt.set_dates("1988-08-01", "2012-12-31")
-bt.set_capital(10000)
-start = time.clock()
-bt._run()
-print("total: ", time.clock() - start, "s\n")
+# # bt = Trader(MomentumStrategy, Portfolio, HistoricalDataHandler, SimulatedExecutionHandler)
+# bt = Trader(BuyAndHold, Portfolio, HistoricalDataHandler, SimulatedExecutionHandler)
+# # bt.set_tickers(["CWI", "HYG", "IAU", "ITB", "SMH", "TLT", "VGK", "VOO", "XBI", "XLI", "XLV"])
+# bt.set_tickers(['VFINX'])
+# bt.set_dates("1988-08-01", "2012-12-31")
+# bt.set_capital(10000)
+# start = time.clock()
+# bt._run()
+# print("total: ", time.clock() - start, "s\n")
 
 
-# with DataManager("data/test.db") as dm:
-#     # data = dm.get_data(["CWI", "HYG", "IAU", "ITB", "SMH", "TLT", "VGK", "VOO", "XBI", "XLI", "XLV", "IGSB"], "2016-01-01", "2017-02-28")
-#     data = dm.get_data(["VFINX"], "1988-08-01", "2012-12-31")
-#
-# print(data.head())
+with DataManager("data/test.db") as dm:
+    # data = dm.get_data(["CWI", "HYG", "IAU", "ITB", "SMH", "TLT", "VGK", "VOO", "XBI", "XLI", "XLV", "IGSB"], "2016-01-01", "2017-02-28")
+    data = dm.get_data(["AAPL"], "1988-08-01", "2018-10-01")
+
+print(data.head())
