951fe0751fb8fb4702ec1ed4642e6a67002de2fc
wonalbe <albertium.land@gmail.com>
2019-01-07 23:16:12 -0500

    add risk_premia code

--- risk_premia.py
+++ risk_premia.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import numpy as np
 from scipy.stats import norm
 from matplotlib import pyplot as plt
 
@@ -24,6 +25,21 @@ def combine_weights(w1, w2, alpha=1):
     return new_w.div(new_w.sum(axis=1), axis=0)
 
 
+def convert_frequency(weights, freq='ME'):
+    if freq == 'ME':
+        return weights.resample('BM').asfreq().resample('B').pad()
+    else:
+        raise ValueError(f'Unrecognized frequency {freq}')
+
+
+def get_performance(prices, weights):
+    monthly_weights = convert_frequency(weights)
+    ret = prices.shift(-1) / prices - 1
+    portfolio_ret = (ret * monthly_weights).dropna().sum(axis=1)
+    annual_ret = np.power((portfolio_ret + 1).prod(), 252 / portfolio_ret.shape[0])
+    return annual_ret - 1
+
+
 tickers = ['VTI', 'EFA', 'IEF', 'TLT']
 with DataManager('./data/test.db') as db:
     data = db.get_data(tickers, '2017-01-01')
@@ -31,6 +47,7 @@ with DataManager('./data/test.db') as db:
 w1 = get_inverse_vol_weights(data.close)
 w2 = get_correlation_adjustement(data.close, 14)
 final_w = combine_weights(w1, w2)
-print(w1.tail(3))
-print(w2.tail(3))
-print(final_w.tail(3))
+print(get_performance(data.close, final_w))
+# print(w1.tail(3))
+# print(w2.tail(3))
+# print(final_w.tail(3))
